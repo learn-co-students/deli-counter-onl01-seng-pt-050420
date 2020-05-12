e53d85843e1088c3fa4b5ec51aaa8a808c73f824
Lantz Warrick <betalantz@gmail.com>
2020-05-12 23:14:44 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,6 @@ def now_serving(deli)
   if deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{deli[0]}."
-    deli.shift
+    puts "Currently serving #{deli.shift}."
   end
 end
