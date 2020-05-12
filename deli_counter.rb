b5cc7a515d3a621277ba28dfd03e20d3d3252806
Victor Williams <victor.williams1030@gmail.com>
2020-05-12 14:06:56 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,24 @@
 def line(array)
-katz_deli = []
+  katz_deli_line = []
   if array.length == 0 
     puts "The line is currently empty."
+  else 
+    array.each_with_index { |name, index| katz_deli_line.push("#{index + 1}. #{name}") }
+    puts "The line is currently: #{katz_deli_line.join(" ")}"
   end
 end
 
-def take_a_number(line)
-katz_deli = []
-  line.each_with_index 
-    puts "The line is currently: #{index + 1}. #{name}"
-  end
-end
+def take_a_number(customer_line, name)
+  customer_line.push(name)
+  puts "Welcome, #{name}. You are number #{customer_line.length} in line."
+end 
+
+def now_serving(customer_line)
+  if customer_line.length === 0 
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{customer_line.shift}."
+  end #conditionals!!
+end #now_serving method!!
+
+#I'm so glad to be finished with this lab!
