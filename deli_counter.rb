8aa6259ac9d02919f750ad48b930f016d068d935
Victor Williams <victor.williams1030@gmail.com>
2020-05-12 00:38:29 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(array)
+katz_deli = []
+  if array.length == 0 
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(line)
+katz_deli = []
+  line.each_with_index 
+    puts "The line is currently: #{index + 1}. #{name}"
+  end
+end
